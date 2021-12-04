d087ffac0651398d1301e9e56ce9942bc90dcb69
ArcanSider <arcansider@gmail.com>
2021-12-04 19:15:57 +0000

    website upd

--- src/resources/views/contactus.blade.php
+++ src/resources/views/contactus.blade.php
@@ -1,11 +1,14 @@
 @extends('layout')
-
+<title>Contact us</title>
 
 @section('main_content')
 
 
     <h1>Contact us</h1>
-    <p>fslgfl;dgd;</p>
+    <h3>Do you have a question?</h3>
+    <p>Contact us via e-mail leaveyourcomment@gmail.com</p>
+    <h3>Do you need a quickly response?</h3>
+    <p>Call us! +446654345761</p>
 
 
 @endsection
--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,7 +1,15 @@
 @extends('layout')
 
-@section('title')Home@endsection
+<title>Home</title>
 
 @section('main_content')
-<p>Hello guys</p>
+    <h1>Here you can leave your comment!</h1>
+    <form method="post" action="/home/check">
+        <input type="email" name="email" id="email" placeholder="Type your valid e-mail" class="form-control"><br>
+        <input type="text" name="subject" id="subject" placeholder="Type the topic" class="form-control"><br>
+        <textarea name="message" id="message" class="form-control" placeholder="Type your comment"></textarea><br>
+        <button type="submit" class="btn btn-success">Send</button>
+
+
+    </form>
 @endsection
